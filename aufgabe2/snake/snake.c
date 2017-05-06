7fd8c6dd808450d105c2de33823fb8d88b2463e4
Deltachi <dominik.scheffler@me.com>
2017-05-06 14:06:12 +0000

    simple for-loop structure to print the current frame

--- aufgabe2/snake/snake.c
+++ aufgabe2/snake/snake.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <support.h>
+#include "support.h"
 
-#define BLANK 0
-#define HEAD 1
-#define TAIL 2
-#define FOOD 9
-#define KEY_MOVE_UP 119
-#define KEY_MOVE_LEFT 97
-#define KEY_MOVE_DOWN 115
-#define KEY_MOVE_RIGHT 100
-#define WIDTH 60
-#define HEIGHT 20
-#define DEBUG 0
+#define KEY_UP 119		//W
+#define KEY_LEFT 97		//A
+#define KEY_DOWN 115	//S
+#define KEY_RIGHT 100	//D
+
+#define BLANK 0			//empty field
+#define HEAD 1			//snake head symbol
+#define TAIL 2			//snake tail symbol
+#define FOOD 9			//food item symbol
+
+#define WIDTH 60		//plane size X
+#define HEIGHT 20		//plane size Y
+#define DEBUG 0			//for debugging if-conditions
 
 void printScreen(){
-	
+	int x,y = 0;
+	for (y = 0; y<HEIGHT; y++){
+		for (x = 0; x<WIDTH; x++){
+			
+		}
+	}
 }
 
 
 int main(int argc, char const *argv[]){
-        
+        printf("Hello World!");
+        return 0;
 }
