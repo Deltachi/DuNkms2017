667a681fe986967b8b87ce3071e8c1e2aafa0633
Deltachi <dominik.scheffler@me.com>
2017-05-06 13:51:05 +0000

    minor changes

--- aufgabe2/snake/snake.c
+++ aufgabe2/snake/snake.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <support.h>
+
+#define BLANK 0
+#define HEAD 1
+#define TAIL 2
+#define FOOD 9
+#define KEY_MOVE_UP 119
+#define KEY_MOVE_LEFT 97
+#define KEY_MOVE_DOWN 115
+#define KEY_MOVE_RIGHT 100
+#define WIDTH 60
+#define HEIGHT 20
+#define DEBUG 0
+
+void printScreen(){
+	
+}
 
 
 int main(int argc, char const *argv[]){
