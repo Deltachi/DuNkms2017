1d3d0794d48ed28fc1ab5dfdf43824a50d21adac
Deltachi <dominik.scheffler@me.com>
2017-05-06 14:25:02 +0000

    placeholder, new constants new struct for tail

--- aufgabe2/snake/snake.c
+++ aufgabe2/snake/snake.c
@@ -7,14 +7,21 @@
 #define KEY_DOWN 115	//S
 #define KEY_RIGHT 100	//D
 
+//field filler
 #define BLANK 0			//empty field
 #define HEAD 1			//snake head symbol
 #define TAIL 2			//snake tail symbol
 #define FOOD 9			//food item symbol
-
-#define WIDTH 60		//plane size X
-#define HEIGHT 20		//plane size Y
+//field parameter
+#define WIDTH 60		//field size X
+#define HEIGHT 20		//field size Y
+//etc
 #define DEBUG 0			//for debugging if-conditions
+//direction values
+#define N 0				//north or up
+#define E 1				//east  or right
+#define S 2				//south or down
+#define W 3				//west  or left
 
 int field[WIDTH][HEIGHT];
 
@@ -22,6 +29,21 @@ struct position {
 	int x;
 	int y;
 } pos;
+struct tail_position {
+	int x;
+	int y;
+	int direction;
+} tail_pos;
+
+void movePlayer();
+void createItem();
+void calculateField();
+void printScreen();
+
+int main(int argc, char const *argv[]){
+        printf("Hello World!");
+        return 0;
+}
 
 void printScreen(){
 	int x,y = 0;
@@ -30,10 +52,4 @@ void printScreen(){
 			
 		}
 	}
-}
-
-
-int main(int argc, char const *argv[]){
-        printf("Hello World!");
-        return 0;
 }
